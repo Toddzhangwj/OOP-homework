29a18975db7a740495b62cec1aaffc8ac5359bd2
Toddzhangwj <1347189391@qq.com>
2022-11-10 22:23:26 +0800

    生成chess类

---
+++ src/Chess.java
@@ -0,0 +1,26 @@
+public class Chess {
+    private int x;//行
+    private int y;//列
+
+    //构造函数
+    public Chess(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
