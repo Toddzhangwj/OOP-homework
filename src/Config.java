82251ccd64133be97bbb49f532926f1a0784376e
Toddzhangwj <1347189391@qq.com>
2022-11-10 22:07:07 +0800

    棋盘基本信息

---
+++ src/Config.java
@@ -0,0 +1,8 @@
+public class Config {
+    public int X0=20; //棋盘起点坐标x0
+    public int Y0=30;  //棋盘起点坐标y0
+    public int ROWS=19;  //行数
+    public int COLUMNS =19;  //列数
+    public int CHESS_SIZE=30;   //棋子的大小
+    public int SIZE=30;      //棋盘行与行  列与列之间的距离
+}
